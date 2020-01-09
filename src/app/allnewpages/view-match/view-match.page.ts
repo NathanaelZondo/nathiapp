71f749958fb03dff33bc0e56164d427774d15912
Keketso Matsuma <49682202+MatsumaKeketso@users.noreply.github.com>
2020-01-09 12:04:28 +0200

    View tourn, match, player and team done

--- src/app/allnewpages/view-match/view-match.page.ts
+++ src/app/allnewpages/view-match/view-match.page.ts
@@ -1,6 +1,6 @@
 import { NavController } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 import * as firebase from 'firebase'
 @Component({
   selector: 'app-view-match',
@@ -8,17 +8,95 @@ import * as firebase from 'firebase'
   styleUrls: ['./view-match.page.scss'],
 })
 export class ViewMatchPage implements OnInit {
+  segmentVal = 'lineup'
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  viewPlayer = false;
+  playerDetailsDiv = document.getElementsByClassName('playerInfo')
+  viewTeam = false;
+  teamDetailsDiv = document.getElementsByClassName('teamInfo')
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>BACK END>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
   viewingMatch = null
   db = firebase.firestore()
-  constructor(public navCtrl: NavController, private route: ActivatedRoute,private router: Router) { }
+  match
+  tournament
+  constructor(public navCtrl: NavController, private route: ActivatedRoute, private router: Router,public renderer: Renderer2) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
-        this.viewingMatch = this.router.getCurrentNavigation().extras.state.parms
-        // 
-        
-      }});
-  }
+        this.match = this.router.getCurrentNavigation().extras.state.parms
+        this.tournament = this.router.getCurrentNavigation().extras.state.tournament
+        //tournament
+        console.log(this.tournament);
 
+
+      }
+    });
+  }
+  segmentChanged(ev) {
+    console.log(ev.detail.value)
+  }
+  goBack() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        parms: this.tournament,
+
+      }
+    };
+    this.navCtrl.navigateBack('view-tournament', navigationExtras)
+  }
+  player(side, state) {
+    switch (state) {
+      case 'open':
+        if (side=='home') {
+          this.viewPlayer = true;
+          this.renderer.setStyle(this.playerDetailsDiv[0],'display','flex')
+        } else {
+          this.viewPlayer = true;
+          this.renderer.setStyle(this.playerDetailsDiv[0],'display','flex')
+          
+        }
+        break;
+      case 'close':
+        if (side=='home') {
+          this.viewPlayer = false;
+          setTimeout(() => {
+            this.renderer.setStyle(this.playerDetailsDiv[0],'display','none')
+          }, 500);
+        } else {
+          this.viewPlayer = false;
+          setTimeout(() => {
+            this.renderer.setStyle(this.playerDetailsDiv[0],'display','none')
+          }, 500);
+        }
+        break;
+    }
+  }
+  team(side, state) {
+    switch (state) {
+      case 'open':
+        if (side=='home') { 
+          this.viewTeam = true;
+          this.renderer.setStyle(this.teamDetailsDiv[0],'display','flex')
+        } else {
+          this.viewTeam = true;
+          this.renderer.setStyle(this.teamDetailsDiv[0],'display','flex')
+        }
+        break;
+      case 'close':
+        if (side=='home') {
+          this.viewTeam = false;
+          setTimeout(() => {
+            this.renderer.setStyle(this.teamDetailsDiv[0],'display','none')
+          }, 500);
+        } else {
+          this.viewTeam = false;
+          setTimeout(() => {
+            this.renderer.setStyle(this.teamDetailsDiv[0],'display','none')
+          }, 500);
+        }
+        break;
+    }
+  }
 }
--- src/app/allnewpages/view-tournament/view-tournament.page.ts
+++ src/app/allnewpages/view-tournament/view-tournament.page.ts
@@ -12,19 +12,25 @@ export class ViewTournamentPage implements OnInit {
   viewedTournament = null
   tournMatches = []
   db = firebase.firestore()
-  constructor(public navCtrl: NavController, private route: ActivatedRoute,private router: Router) { }
+  constructor(public navCtrl: NavController, private activatedRoute: ActivatedRoute,private router: Router) { }
 
   ngOnInit() {
     // receives nav params
-    this.route.queryParams.subscribe(params => {
+    
+    this.activatedRoute.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.viewedTournament = this.router.getCurrentNavigation().extras.state.parms
         console.log(this.viewedTournament);
-        this.db.collection('MatchFixtures').where('tournid','==',this.viewedTournament.doc_id).orderBy("startDate", "desc").get().then(res => {
+        this.db.collection('MatchFixtures').where('tournid','==',this.viewedTournament.doc_id).orderBy("matchdate", "desc").get().then(res => {
+          console.log(res)
+          this.tournMatches = []
           res.forEach(doc => {
             this.tournMatches.push(doc.data())
           })
           console.log(this.tournMatches);
+          
+        }).catch(err => {
+          console.log(err);
           
         })
       }});
@@ -37,7 +43,8 @@ export class ViewTournamentPage implements OnInit {
   viewMatch(match) {
     let navigationExtras: NavigationExtras = {
       state: {
-        parms:match
+        parms:match,
+        tournament: this.viewedTournament
       }
     };
     // passes nav params
